ff59dca3da6788436c21453b2ea5d7ed1da0ba13
jlamby <lamby.julien@gmail.com>
2020-05-12 14:47:35 +0200

    Rename unit tests in a more behavioral maner

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class YatzyTest {
 
     @Test
-    public void chance_scores_sum_of_all_dice() {
+    public void chance_scoring_category_should_sum_all_dice() {
         int expected = 15;
         int actual = Yatzy.chance(2, 3, 4, 5, 1);
         assertEquals(expected, actual);
@@ -14,7 +14,7 @@ public class YatzyTest {
     }
 
     @Test
-    public void yatzy_scores_50() {
+    public void yatzy_scoring_category_should_return_50_if_all_dices_have_the_same_number() {
         int expected = 50;
         int actual = Yatzy.yatzy(4, 4, 4, 4, 4);
         assertEquals(expected, actual);
@@ -23,7 +23,7 @@ public class YatzyTest {
     }
 
     @Test
-    public void test_1s() {
+    public void ones_scoring_category_should_sum_all_dices_with_one() {
         assertTrue(Yatzy.ones(1, 2, 3, 4, 5) == 1);
         assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
         assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
@@ -31,81 +31,81 @@ public class YatzyTest {
     }
 
     @Test
-    public void test_2s() {
+    public void twos_scoring_category_should_sum_all_dices_with_two() {
         assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
         assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
     }
 
     @Test
-    public void test_threes() {
+    public void threes_scoring_category_should_sum_all_dices_with_three() {
         assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
         assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
     }
 
     @Test
-    public void fours_test() {
+    public void fours_scoring_category_should_sum_all_dices_with_four() {
         assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
         assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
         assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
     }
 
     @Test
-    public void fives() {
+    public void fives_scoring_category_should_sum_all_dices_with_five() {
         assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
         assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
         assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
     }
 
     @Test
-    public void sixes_test() {
+    public void sixes_scoring_category_should_sum_all_dices_with_six() {
         assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
         assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
         assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
     }
 
     @Test
-    public void one_pair() {
+    public void pair_scoring_category_should_return_sum_of_the_two_highest_matching_pair_of_dice() {
         assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
         assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
         assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
     }
 
     @Test
-    public void two_Pair() {
+    public void two_pair_scoring_category_should_return_sum_of_the_two_pairs_of_dice_with_the_same_number() {
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
     }
 
     @Test
-    public void three_of_a_kind() {
+    public void three_of_kind_scoring_category_should_return_sum_of_three_dice_with_the_same_number() {
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
         assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
     }
 
     @Test
-    public void four_of_a_knd() {
+    public void four_of_kind_scoring_category_should_return_sum_of_four_dice_with_the_same_number() {
         assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
         assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3));
     }
 
     @Test
-    public void smallStraight() {
+    public void smallStraight_scoring_category_should_return_15_when_dice_number_are_from_one_to_five() {
         assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
         assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
         assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
     }
 
     @Test
-    public void largeStraight() {
+    public void largeStraight_scoring_category_should_return_20_when_dice_number_are_from_two_to_six() {
         assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
         assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
         assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
     }
 
     @Test
-    public void fullHouse() {
+    public void fullHouse_scoring_category_should_return_the_sum_of_all_dice_when_the_dice_are_two_of_a_kind_and_three_of_a_kind() {
         assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
         assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
     }
